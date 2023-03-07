49576c002ef8f7592c0da3bb868e2bf095ef1f84
redlik <ralphr@outlook.com>
2023-03-07 16:50:06 +0000

    Moved tours template

--- index.php
+++ index.php
@@ -16,16 +16,17 @@ get_header();
 
 $is_elementor_theme_exist = function_exists( 'elementor_theme_do_location' );
 
+if ( is_singular('tours') ) {
+	if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'single' ) ) {
+		get_template_part( 'template-parts/single-tours' );
+	}
+}
+
 if ( is_singular() ) {
 	if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'single' ) ) {
 		get_template_part( 'template-parts/single' );
 	}
-} 
-//if ( is_singular('tours') ) {
-//	if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'single' ) ) {
-//		get_template_part( 'template-parts/single-tours' );
-//	}
-//}
+}
 elseif ( is_archive() || is_home() ) {
 	if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'archive' ) ) {
 		get_template_part( 'template-parts/archive' );
