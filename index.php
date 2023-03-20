ad87d7ff7b576dc715fe1db8ea95438ed5703b40
redlik <ralphr@outlook.com>
2023-03-20 19:28:25 +0000

    Fixed index error

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
-//$is_elementor_theme_exist = function_exists( 'elementor_theme_do_location' );
+$is_elementor_theme_exist = function_exists( 'elementor_theme_do_location' );
 //
 //if ( is_singular('tours') ) {
 //	if ( ! $is_elementor_theme_exist || ! elementor_theme_do_location( 'single' ) ) {
